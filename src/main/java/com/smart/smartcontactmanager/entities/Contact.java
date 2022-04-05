bcd0e7a9b435846039ab8c410918b093eb9436ee
suman-stha <shresthasuman919@gmail.com>
2022-04-05 01:24:41 -0400

    first commit

---
+++ src/main/java/com/smart/smartcontactmanager/entities/Contact.java
@@ -0,0 +1,126 @@
+package com.smart.smartcontactmanager.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONTACT")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cId;
+    private String name;
+    private String secondName;
+    private String work;
+    private String email;
+    private String phone;
+    private String image;
+
+    @ManyToOne
+    private User user;
+
+    @Column(length = 1000)
+    private String description;
+
+    public int getcId() {
+        return cId;
+    }
+
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Contact() {
+    }
+
+    public Contact(int cId, String name, String secondName, String work, String email, String phone, String image,
+            User user, String description) {
+        this.cId = cId;
+        this.name = name;
+        this.secondName = secondName;
+        this.work = work;
+        this.email = email;
+        this.phone = phone;
+        this.image = image;
+        this.user = user;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [cId=" + cId + ", description=" + description + ", email=" + email + ", image=" + image
+                + ", name=" + name + ", phone=" + phone + ", secondName=" + secondName + ", user=" + user + ", work="
+                + work + "]";
+    }
+
+}
