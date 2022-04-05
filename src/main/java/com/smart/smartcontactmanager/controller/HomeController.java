db4092cf93746aab6d382873adbb71de7e102a1c
suman-stha <shresthasuman919@gmail.com>
2022-04-05 01:34:56 -0400

    title

--- src/main/java/com/smart/smartcontactmanager/controller/HomeController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.smart.smartcontactmanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "Home-Smart Contact Manger");
+
         return "home";
 
     }
