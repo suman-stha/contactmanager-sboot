57b72566bef6441b712ac77b2c37b9f21d734f65
suman-stha <shresthasuman919@gmail.com>
2022-04-14 12:53:29 -0400

    responsive sidebar

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -1,14 +1,31 @@
 package com.smart.smartcontactmanager.controller;
 
+import java.security.Principal;
+
+import com.smart.smartcontactmanager.dao.UserRepository;
+import com.smart.smartcontactmanager.entities.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping("/index")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {// principal coming from spring security
+
+        String userName = principal.getName();
+        System.out.println("USERNAME " + userName);
+        // get the user name(Email)
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("USER " + user);
+
+        model.addAttribute("user", user);
 
         return "normal/user_dashboard";
     }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,17 @@
 console.log("This is script file.")
+
+const toggleSidebar = () => {
+    if ($('.sidebar').is(':visible')) {
+
+        //true
+        //need to close
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%")
+    }
+    else {
+        //false
+        //need to show
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%")
+    }
+};
