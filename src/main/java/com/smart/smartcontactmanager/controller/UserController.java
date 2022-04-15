81d3107ad37fa52b98bb5e26c1dc1db6b1e392ce
suman-stha <shresthasuman919@gmail.com>
2022-04-15 01:42:31 -0400

    Designingi Contact form

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -3,11 +3,14 @@ package com.smart.smartcontactmanager.controller;
 import java.security.Principal;
 
 import com.smart.smartcontactmanager.dao.UserRepository;
+import com.smart.smartcontactmanager.entities.Contact;
 import com.smart.smartcontactmanager.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +19,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal) {// principal coming from spring security
+    // method for adding common data to response
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
 
         String userName = principal.getName();
         System.out.println("USERNAME " + userName);
@@ -27,7 +31,21 @@ public class UserController {
 
         model.addAttribute("user", user);
 
+    }
+
+    // dashboard home
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal) {// principal coming from spring security
+        model.addAttribute("title", "Home Page");
         return "normal/user_dashboard";
     }
 
+    // open add form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
+
+    }
 }
