279215b2913a34159fa09bcb4e21610c1a05e51b
suman-stha <shresthasuman919@gmail.com>
2022-04-21 01:08:59 -0400

    solving security bug and sendingContactNameOnTitle

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -135,11 +135,22 @@ public class UserController {
 
     // showing particular contact details
     @RequestMapping("/{cId}/contact")
-    public String showContactDetail(@PathVariable("cId") Integer cId, Model model) {
+    public String showContactDetail(@PathVariable("cId") Integer cId, Model model, Principal principal) {
         System.out.println("CID" + cId);
         Optional<Contact> contactOptional = this.contactRepository.findById(cId);
         Contact contact = contactOptional.get();
-        model.addAttribute("contact", contact);
+
+        //
+        String userName = principal.getName();
+        User user = this.userRepository.getUserByUserName(userName);
+
+        if (user.getId() == contact.getUser().getId()) {
+
+            model.addAttribute("contact", contact);
+            model.addAttribute("title", contact.getName());
+
+        }
+
         return "normal/contact_detail";
     }
 }
